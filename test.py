937fffade7fb80534de6d2fd1d48948e92e028cf
BenjaminC188 <benjaminc188@nycstudents.net>
2025-10-06 06:24:35 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,45 @@
-import random
+"""import random
+a = 0
 def NumberGuessingGame(y):
     x = random(1-10)
-    for i in range():
-        if x == y:
-            print("You've won!")
-        elif x > y:
-            print("Too low!")
-        elif x < y:
-            print("Too high!")
+    while():
+    x > y
+    print("Too low")
+    elif x < y:
+    print("Too high")
+    y == x
+    print("You've won!") and print(guess_history)
 
-NumberGuessingGame(5)
 
-"""def guess_history():
+
+
+
+def guess_history():
     for i in range():
-        print(imput)"""
+        print(y)"""
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def lang(x):
+    for char in x:
+        print(char)
+        if t and T in char < s and S in char:
+            print
+lang("One Two three")
+
