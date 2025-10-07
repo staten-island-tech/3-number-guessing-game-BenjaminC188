c0efd724284e0ff5543b9f42425611cf314c2ae9
BenjaminC188 <benjaminc188@nycstudents.net>
2025-10-07 17:56:11 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,21 @@
 import random
-x = 0
-y = random.randint(1, 10)
-def number_guessing_game(y):
+
+def number_guessing_game():
+    y = random.randint(1, 10)
+    x = 0
+    guesses = []
     while x != y:
-        x = int(input("Guess a number between 1 and 10."))
+        x = int(input("Guess a number 1 to 10"))
+        guesses.append(x)
         if x < y:
             print("Too low")
         elif x > y:
             print("Too high")
         else:
-            print("You guessed the number")
+            print("You guessed the number!")
+            print(guesses)
+
+number_guessing_game()
 
 
 
@@ -27,11 +33,22 @@ def number_guessing_game(y):
 
 
 
-
-"""def lang(x):
+def lang(x):
+    t = 0
+    s = 0
     for char in x:
         print(char)
-        if t and T in char < s and S in char:
-            print
-lang("One Two three")"""
+        if char == "t" or char == "T":
+            t += 1
+        elif char == "s" or char == "S":
+            s += 1
+    if t > s:
+        print("English")
+    else:
+        print("French")
+lang("One Two three")
 
+def spaces(y,t):
+    for char in y:
+        if char in t:
+spaces("CCC..", ".C.C.C")
